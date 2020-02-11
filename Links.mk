8d53f2995f5976b0fd2af11d13f52138acdf8766
David <40766399+hamdav@users.noreply.github.com>
2020-02-11 21:43:43 +0100

    Länkar till bra sidor

---
+++ Links.mk
@@ -0,0 +1,3 @@
+[Vad är en databas?](https://www.guru99.com/introduction-to-database-sql.html)
+[Hur gör man en MySQL-databas från command line](https://www.wikihow.com/Create-a-Table-in-MySQL)
+[Java-mySQL interface (stack overflow)](https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database)
